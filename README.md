20b7859126be9d381e1326b4fee12c08b6caf1d2
Daniel Lee <daniel4lee@gmail.com>
2019-09-17 19:10:29 +0800

    finish README

--- README.md
+++ README.md
@@ -8,13 +8,36 @@ A practice of Nvlab summer school.
 
 ## Introduction
 
-* __Histogram Equalization__: Create the histogram of grey scale value for image, then forming the table of correspounding cumulative distribution function. And, to generate the equalized image, we only need to subtitute the original gray scale value into the *general histogram equalization formula* as below.
-![general histogram equalization formula](https://i.imgur.com/uAKEVap.png)
-where cdf-min is the minimum non-zero value, M × N gives the image's number of pixels, and L is the number of grey levels used (in this case 256).
+* __Histogram Equalization__
+ 
+    * __Overview__: This method is to improve the contrast of images by spreading the clustered intensities value over the whole range.
+    ![](https://i.imgur.com/FMpJXSm.png)
+    * __Steps__: 
+        1. Create the histogram of grey scale value for source image.
+        2. Form the table of correspounding cumulative distribution function. 
+        3. Make transition by subtituting the original gray scale value into the *general histogram equalization formula* as below. 
+ ![](https://i.imgur.com/uAKEVap.png)
+             > Note: cdf-min is the minimum non-zero value, M × N gives the image's number of pixels, and L is the number of grey levels used (in this case is 256).
+    * __advantage__: 
+        1. Increases the global contrast of images (especially images have close contrast values).
+        2. Intensities can be better distributed on the histogram (backgrounds and foregrounds are both bright or dark).
+        3. It is a invertible operation.
+        4. Computation is not big.
+    * __disadvantage__:
+        1. It is indiscriminat (may increase the contrast of background noise and decrease the usable signal).
+* __Gamma Correction__
+    * __Overview__: The corrections is to translate between eye's light sensitivity and the camera (twice the light to camera means twice the signial, however to eyes percieves as non-linear relationship).
+    ![Eyes are more sensitive in dark tones than in bright tones](https://i.imgur.com/B5ubC1p.png=200x200)
 
-
-* Gamma Correction
-* Image Inversion
+    * __Definition__: *Vout = Vin^gamma* (*Vout* is the output luminance value and *Vin* is the input/actual luminance value)
+    * __Gamma Workflow__: 
+        1. Image Gamma: This is applied whenever a captured image is converted into JPEG. Redistributing native camera tonal levels into more perceptually uniform one (eyes). Also, making the most efficient use of given bit depth.
+        2. Display Gamma: This refers to compensate for file's gamma, ensuring the image isn't unrealistically brightened when displayed on screen.
+        3. System Gamma: This represents the net effect of all gamma values applied to the image (ideally be close to gamma = 1).
+        ![](https://i.imgur.com/UmyDFGD.png)
+* __Image Inversion__
+    * __Overview__: Colors are reversed into their respective complementary colors. 
+    * The formula is simple `F(X) = COLOR_MAX - COLOR X`.
 
 ## Installation
 
@@ -47,11 +70,18 @@ The application could load image files with `*.png, *.tif, *.bmp, *jpg, and *.ra
 
 ## Dependencies
 
-* [numpy](http://www.numpy.org/)
+* [Numpy](http://www.numpy.org/)
 
-* [opencv-python](https://pypi.org/project/opencv-python/)
+* [Opencv-Python](https://pypi.org/project/opencv-python/)
 
 * [PyQt5](https://riverbankcomputing.com/software/pyqt/intro)
 
+## Reflection
+
+It has been about one year since I write the program last time. So, by doing the program, I not only learned the basic knowledge of image process but got familiar with coding. However, I think there still are many points which I can improve, such as coding style. I should keep on learning and refining my ability.
+
 ## Reference
-* [__Wiki Histogram equalization__](https://en.wikipedia.org/wiki/Histogram_equalization)
+
+* [__Wiki Histogram Equalization__](https://en.wikipedia.org/wiki/Histogram_equalization)
+* [__Wiki Gamma Correction__](https://en.wikipedia.org/wiki/Gamma_correction)
+* [__Cambridge in Color__](https://www.cambridgeincolour.com/tutorials/gamma-correction.htm)
--- main.py
+++ main.py
@@ -1,10 +1,212 @@
+import math
 import sys
-from PyQt5.QtWidgets import QApplication
-from Gui.gui import GuiRoot
+import os
+from os.path import join, isfile
+
+import cv2 as cv
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import QCoreApplication, QObject, QRunnable, QThread, QThreadPool, pyqtSignal, pyqtSlot 
+from PyQt5.QtGui import QIntValidator, QDoubleValidator, QPixmap, QImage
+import numpy
+
+class GuiRoot(QWidget):
+    # Root of Gui
+    def __init__(self):
+        super().__init__()     
+        self.ui_init()
+
+    def ui_init(self):
+        # Set window
+        self.file_gui()
+        self.process_image_gui()
+        self.setFixedSize(1080, 560) # due to unfix image size
+        self.center()
+        self.setWindowTitle('Hw1')
+        self.img = numpy.ndarray(())
+        self.image_label = QLabel() # label for image display
+        self.image_label.resize(960, 539)
+        
+        self.original_flag = False
+        # control panel layout
+        hbox = QHBoxLayout()
+        vbox = QVBoxLayout()
+
+        vbox.addWidget(self.file_qgroupbox)
+        vbox.addWidget(self.process_qgroupbox)
+        vbox.setStretchFactor(self.file_qgroupbox, 1)
+        vbox.setStretchFactor(self.process_qgroupbox, 3)
+        vbox.insertSpacing(-1, 280)
+        hbox.addLayout(vbox)
+        hbox.addWidget(self.image_label)
+        hbox.setStretchFactor(self.image_label, 3)
+        self.setLayout(hbox)
+        self.show()
+
+    def file_gui(self):
+        self.file_qgroupbox = QGroupBox("File: ")
+        layout = QGridLayout()
+
+        # open image related gui
+        self.open_image_btn = QPushButton("Open Image", self)
+        self.open_image_btn.clicked.connect(self.open_function)
+
+        # save current image related gui
+        self.save_image_btn = QPushButton("Save Image", self)
+        self.save_image_btn.clicked.connect(self.save_function)
+        
+        layout.addWidget(self.open_image_btn, 0, 0, 1, 1)
+        layout.addWidget(self.save_image_btn, 0, 1, 1, 1)
+        self.file_qgroupbox.setLayout(layout)
+        
+    def process_image_gui(self):
+        self.process_qgroupbox = QGroupBox("Image Process: ")
+        layout = QGridLayout()
+        layout.setSpacing(50)
+        # process image related gui
+        self.HE_btn = QPushButton("Histogram Equalization", self)
+        self.HE_btn.clicked.connect(self.histogram_equation_function)
+        layout.addWidget(self.HE_btn, 0, 0, 1, 2)
+
+        self.Inver_btn = QPushButton("Image Inversion", self)
+        self.Inver_btn.clicked.connect(self.inverse_function)
+        layout.addWidget(self.Inver_btn, 1, 0, 1, 2)
+
+        self.GC_btn = QPushButton("Gamma Correction", self)
+        self.GC_btn.clicked.connect(self.GC_function)
+        self.GC_degree = QDoubleSpinBox()
+        self.GC_degree.setDecimals(2)
+        self.GC_degree.setValue(0.22)
+        self.GC_degree.setRange(0,10)
+        layout.addWidget(self.GC_btn, 2, 0, 1, 1)
+        layout.addWidget(self.GC_degree, 2, 1, 1, 1)
+
+        self.Ori_btn = QPushButton("Original image", self)
+        self.Ori_btn.clicked.connect(self.origin_function)
+        layout.addWidget(self.Ori_btn, 3, 0, 1, 2)        
+
+        layout.setVerticalSpacing(0)
+        layout.setHorizontalSpacing(0)
+        self.process_qgroupbox.setLayout(layout)
+
+    def open_function(self):
+        # open image function
+        datapath = os.path.realpath(os.path.join(os.getcwd(), "ImageData")) # the default location
+        file_name = QFileDialog.getOpenFileName(self, 'Open image', 
+        datapath, "Image files(*.jpg *.png *.tif *.bmp *.raw)") # support image format
+        
+        if file_name[0]:
+            self.img = cv.imread(file_name[0], -1)
+        
+            if 'raw' in file_name[0]:
+                # imread function is not support raw file
+                self.img = numpy.fromfile(file_name[0], dtype=numpy.uint8)
+                self.img = self.img.reshape(512, 512)
+                self.img = numpy.stack((self.img,)*3, axis=-1) # stack gray scale img for rgb 3 channels
+            
+            self.original_img = numpy.copy(self.img)
+            self.original_flag = True
+            self.show_function(self.img)
+
+    def show_function(self, img):
+        # get the size and channel of image
+        height, width, channel = img.shape
+        bytesPerLine = channel*width
+
+        # turn image of opencv into Qimage
+        qImg = QImage(img.data, width, height, bytesPerLine,
+            QImage.Format_RGB888).rgbSwapped()
+        # show Qimage on label
+        self.image_label.setPixmap(QPixmap.fromImage(qImg))
+
+    def save_function(self):
+        # save current image function
+        file_name = QFileDialog.getSaveFileName(
+            self, 'Save Image', './save_img', 'Image files(*.jpg *.png* .tif *.raw *.bmp)')
+        if file_name[0]:
+            cv.imwrite(file_name[0], self.img)
+
+    def inverse_function(self):
+        # use 255(the largest value of color) to minus the current value
+        # by doing so, the image become negative
+        if self.original_flag:
+            self.img = cv.cvtColor(self.img, cv.COLOR_BGR2GRAY)
+            self.img = 255 - self.img
+            self.img = numpy.stack((self.img,)*3, axis=-1)
+            self.show_function(self.img)
+    
+    def histogram_equation_function(self):
+        def transform_function(cdf, height, width):
+            # wiki: https://tinyurl.com/yxjfwskj
+            cdf_min = numpy.amin(cdf) # get the minimun in cdf
+            table = [0]*256 # table for 256 scale
+            for i in range(256):
+            # fill in the table with general histogram equalization formula
+                table[i] = round(255 * (cdf[i] - cdf_min) 
+                / (height * width -cdf_min), 0)
+            
+            for i in range(height):
+            # look up the table for transition
+                for j in range(width):
+                    self.img[i, j] = table[self.img[i, j]]
+        if self.original_flag:
+            self.img = cv.cvtColor(self.img, cv.COLOR_BGR2GRAY) # turn to gray scale
+            height, width = self.img.shape
+
+            histogram = [0]*256 # create the histogram
+            for h in range(height):
+                for w in range(width):
+                    histogram[self.img[h, w]] += 1
+        
+            cdf = [0]*256 # calculate cumulative distribution function
+            for i in range(len(histogram)):
+                cdf[i] = sum(histogram[:i+1])
+            cdf = numpy.array(cdf)
+            '''
+            transform = numpy.uint8(255 * cdf / height / width) #finding transfer function values
+            '''
+            transform_function(cdf, height, width)
+            self.img = numpy.stack((self.img,)*3, axis=-1)
+            self.show_function(self.img)
+    def GC_function(self):
+        # Gamma correction function
+        if self.original_flag:
+            a = float(self.GC_degree.value()) # get Gamma value
+            height, width, channel= self.img.shape 
+            table =  [ [0]*256 for i in range(3)] #create table which could accerlate the computation
+
+            for channel in range(3): 
+                max_value = numpy.amax(self.img[:, :, channel])
+                for index in range(256):
+                    table[channel][index] = numpy.power(index / max_value, a) * max_value
+                    # calculate correspounding gamma correction value for the 0 ~ 255 shades
+
+            for channel in range(3):
+                for h in range(height):
+                    for w in range(width):
+                        self.img[h, w, channel] = table[channel][self.img[h, w, channel]]
+                        # mapping the image by gamma correction table
+            self.img = numpy.uint8(self.img)
+            self.show_function(self.img)
+    def origin_function(self):
+        # return the original image 
+        if not self.original_flag:
+            # haven't open any image file
+            pass
+        else:
+            # reset img and show it
+            self.img = numpy.copy(self.original_img)
+            self.show_function(self.img)
+
+    def center(self):
+        # Place window in the center
+        qr = self.frameGeometry()
+        central_p = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(central_p)
+        self.move(qr.topLeft())
 
 if __name__ == '__main__':
-    
     sys.argv += ['--style', 'fusion']
     app = QApplication(sys.argv)
-    gui_root = GuiRoot() # instance of the gui
+    gui_root = GuiRoot() # instance of the gui and finction
     sys.exit(app.exec_())
